7cb9a37162a37aa5d8bdd08cf1aac1979d851733
江原 圭 <matador.sangria@gmail.com>
2020-06-16 08:03:31 +0900

    refactor: eslint fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,26 +3,26 @@ module.exports = {
   head: {
     title: 'kusabuki',
     htmlAttrs: {
-      lang: 'ja'
+      lang: 'ja',
     },
     meta: [
       { charset: 'utf-8' },
-      { name: 'robots', content: 'noindex , nofollow' },
+      { name: 'robots', content: 'noindex, nofollow' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Nuxt.js project' }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
   styleResources: {
-    scss: ['~/assets/css/_variables.scss']
+    scss: ['~/assets/css/_variables.scss'],
   },
   css: ['~/assets/css/main.scss'],
-  plugins: ['~/plugins/mixin.js'],
+  plugins: ['~/plugins/mixin.ts'],
   modules: ['@nuxtjs/style-resources', 'nuxt-webfontloader'],
   webfontloader: {
     google: {
-      families: ['Crimson+Pro', 'Noto+Serif+JP:wght@400;700']
-    }
+      families: ['Crimson+Pro', 'Noto+Serif+JP:wght@400;700'],
+    },
   },
   build: {
     extend(config, { isDev, isClient }) {
@@ -35,10 +35,10 @@ module.exports = {
           exclude: /(node_modules)/
         });
       }
-    }
+    },
   },
   buildModules: ['@aceforth/nuxt-optimized-images'],
   optimizedImages: {
-    optimizeImages: true
-  }
+    optimizeImages: true,
+  },
 };
--- plugins/mixin.js
+++ plugins/mixin.js
@@ -4,13 +4,13 @@ import VueScrollTo from 'vue-scrollto';
 Vue.use(VueScrollTo, {
   duration: 700,
   easing: [0, 0, 0.1, 1],
-  offset: -80
+  offset: -80,
 });
 
 Vue.mixin({
   methods: {
     $delay(ms) {
-      return new Promise(resolve => setTimeout(resolve, ms));
-    }
-  }
+      return new Promise((resolve) => setTimeout(resolve, ms));
+    },
+  },
 });
